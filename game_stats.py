cf95d8a5e0641ba2d8db59efb3b3e08e08311a26
Sebastian Herman <sebastian.herman@live.com>
2021-01-31 18:50:36 +0200

    scoring

--- game_stats.py
+++ game_stats.py
@@ -6,9 +6,14 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
-        # start alien invasion active
-        self.game_active = True
+        # start alien invasion inactive
+        self.game_active = False
+
+        # high score should never be reset
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize stats that can change during the game"""
         self.ships_left = self.settings.ship_limit
+        self.score = 0
+        self.level = 1
--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
 
         # ship settings
         self.ship_speed = 1.5
-        self.ship_limit = 3
+        self.ship_limit = 2
 
         # bullet settings
         self.bullet_speed = 2.0
@@ -24,3 +24,32 @@ class Settings:
         self.fleet_drop_speed = 10
         # fleet direction of 1 represents right; -1 left
         self.fleet_direction = 1
+
+        # the rate at which the game speeds up
+        self.speedup_scale = 1.1
+
+        # how quickly the alien point values increase
+        self.score_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """Initialize settings that change throughout the game"""
+        self.ship_speed = 1.5
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet direction of 1 represents right; -1 left
+        self.fleet_direction = 1
+
+        # scoring
+        self.alien_points = 50
+
+    def increase_speed(self):
+        """Increase speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+        print(self.alien_points)
